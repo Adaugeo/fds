98bffa479ce2bd1bea1bb0512e0ce719a64ae45c
Adaugeo <strogers@RageBookPro.local>
2016-01-20 18:58:11 -0500

    Maker test

---
+++ Maker test.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html />
+<title>Untitled Document</title>
+</head>
+
+<body>
+
+
+<?php
+
+  DEFINE('DB_USERNAME', 'root');
+ DEFINE('DB_PASSWORD', 'root');
+￼DEFINE('DB_HOST', 'localhost');
+￼DEFINE('DB_DATABASE', 'Maker');
+
+
+$mysqli = new mysqli(DB_HOST, DB_USERNAME,DB_PASSWORD, DB_DATABASE);
+
+￼if (mysqli_connect_error()) {
+     ￼die('Connect Error (' . mysqli_connect_errno()
+￼. ') ' . mysqli_connect_error());
+￼}
+
+echo 'Connected successfully.';
+
+$mysqli->close();
+?>
+
+</body>
+</html>
